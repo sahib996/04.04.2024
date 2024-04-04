21ad4c9761e2c53b80603b6db98d119daebbe45c
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-04-03 17:29:07 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using _04._04._2024.model;
+
+namespace _04._04._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            Manager mgr = new Manager();
+        }
+    }
+}
---
+++ model/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._04._2024.model
+{
+    internal class Employee
+    {
+        protected int salary;
+
+        public Employee()
+        {
+           salary = 500; 
+        }
+
+        public virtual void GetPromotion()
+        {
+            salary += 100; 
+            Console.WriteLine("salary");
+        }
+    }
+}
